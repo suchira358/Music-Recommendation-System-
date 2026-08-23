7978589ca9fdd02c994969caee535168d250d587
suchira358 <suchirareddychalla@gmail.com>
2026-08-24 00:03:49 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -4,53 +4,62 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class MusicRecommender:
+
     def __init__(self, csv_file):
 
         self.df = pd.read_csv(csv_file)
 
         self.features = [
-            "danceability",
-            "energy",
-            "loudness",
-            "speechiness",
-            "acousticness",
-            "instrumentalness",
-            "liveness",
-            "valence",
-            "tempo"
+            "danceability", "energy", "loudness",
+            "speechiness", "acousticness",
+            "instrumentalness", "liveness",
+            "valence", "tempo"
         ]
 
-   
         self.df = self.df.dropna(subset=self.features)
 
         scaler = StandardScaler()
-        scaled_features = scaler.fit_transform(self.df[self.features])
+        features = scaler.fit_transform(self.df[self.features])
 
-        self.similarity = cosine_similarity(scaled_features)
+        self.similarity = cosine_similarity(features)
+
 
     def recommend(self, song_name, top_n=5):
-  
-        song = self.df[
+
+        songs = self.df[
             self.df["track_name"].str.lower() == song_name.lower()
         ]
 
-        if song.empty:
+        if songs.empty:
             return None
 
-        index = song.index[0]
+        index = songs.index[0]
+        mood = self.df.loc[index, "mood"]
 
-        scores = list(enumerate(self.similarity[index]))
+        mood_songs = self.df[
+            self.df["mood"].str.lower() == mood.lower()
+        ]
 
-        scores = sorted(scores, key=lambda x: x[1], reverse=True)
+        scores = []
+
+        for i in mood_songs.index:
+
+            if i != index:
+                scores.append((i, self.similarity[index][i]))
+
+        scores.sort(key=lambda x: x[1], reverse=True)
 
         recommendations = []
-        for i in scores[1:top_n + 1]:
-            row = self.df.iloc[i[0]]
+
+        for i, score in scores[:top_n]:
+
+            row = self.df.loc[i]
 
             recommendations.append({
                 "Song": row["track_name"],
                 "Artist": row["artists"],
-                "Genre": row.get("track_genre", "Unknown")
+                "Genre": row["track_genre"],
+                "Mood": row["mood"]
             })
 
         return recommendations
